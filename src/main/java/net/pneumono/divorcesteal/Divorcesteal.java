c38b7847e01a48eb0bd5abe86f9309523837d637
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-02 12:00:56 +0100

    Add registry class

--- src/main/java/net/pneumono/divorcesteal/Divorcesteal.java
+++ src/main/java/net/pneumono/divorcesteal/Divorcesteal.java
@@ -3,6 +3,7 @@ package net.pneumono.divorcesteal;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
+import net.pneumono.divorcesteal.content.DivorcestealRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +15,7 @@ public class Divorcesteal implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing Divorcesteal");
+		DivorcestealRegistry.registerDivorcestealContent();
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/net/pneumono/divorcesteal/content/DivorcestealRegistry.java
@@ -0,0 +1,22 @@
+package net.pneumono.divorcesteal.content;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.pneumono.divorcesteal.Divorcesteal;
+
+import java.util.function.Function;
+
+public class DivorcestealRegistry {
+
+    protected static <T extends Item> T registerItem(String name, Function<Item.Settings, T> factory, Item.Settings settings) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Divorcesteal.id(name));
+        return Registry.register(Registries.ITEM, key, factory.apply(settings.registryKey(key)));
+    }
+
+    public static void registerDivorcestealContent() {
+
+    }
+}
