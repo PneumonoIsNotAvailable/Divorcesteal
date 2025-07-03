b46363a1d54ba7bfae936f2d515faff31dce49db
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-03 01:05:50 +0100

    Add heart item

--- src/main/java/net/pneumono/divorcesteal/content/DivorcestealRegistry.java
+++ src/main/java/net/pneumono/divorcesteal/content/DivorcestealRegistry.java
@@ -5,11 +5,13 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Rarity;
 import net.pneumono.divorcesteal.Divorcesteal;
 
 import java.util.function.Function;
 
 public class DivorcestealRegistry {
+    public static final HeartItem HEART_ITEM = registerItem("heart", HeartItem::new, new Item.Settings().rarity(Rarity.UNCOMMON));
 
     protected static <T extends Item> T registerItem(String name, Function<Item.Settings, T> factory, Item.Settings settings) {
         RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Divorcesteal.id(name));
---
+++ src/main/java/net/pneumono/divorcesteal/content/HeartItem.java
@@ -0,0 +1,35 @@
+package net.pneumono.divorcesteal.content;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import net.pneumono.divorcesteal.hearts.Hearts;
+
+public class HeartItem extends Item {
+    public HeartItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public boolean hasGlint(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        if (world.isClient()) return ActionResult.SUCCESS_SERVER;
+
+        int addedHearts = Hearts.addHeartsValidated(user, 1, false);
+        if (addedHearts > 0) {
+            // play heart use sound
+            ItemStack stack = user.getStackInHand(hand);
+            user.getItemCooldownManager().set(stack, 20);
+            user.getStackInHand(hand).decrement(1);
+        }
+
+        return ActionResult.SUCCESS_SERVER;
+    }
+}
