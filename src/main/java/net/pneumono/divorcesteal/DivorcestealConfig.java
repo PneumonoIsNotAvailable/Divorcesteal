2ce61a5bb5f233981ccf5c29a559b99f3c96c42c
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-05 17:16:42 +0100

    Fix incorrectly set default config values

--- src/main/java/net/pneumono/divorcesteal/DivorcestealConfig.java
+++ src/main/java/net/pneumono/divorcesteal/DivorcestealConfig.java
@@ -4,8 +4,8 @@ import net.pneumono.pneumonocore.config.*;
 
 public class DivorcestealConfig {
     public static final IntegerConfiguration MAX_HEARTS = new IntegerConfiguration(id(), "max_hearts", ConfigEnv.SERVER, 1, 100, 20);
-    public static final IntegerConfiguration DEFAULT_HEARTS = new IntegerConfiguration(id(), "default_hearts", ConfigEnv.SERVER, 1, 100, 20);
-    public static final IntegerConfiguration REVIVE_HEARTS = new IntegerConfiguration(id(), "revive_hearts", ConfigEnv.SERVER, 1, 100, 20);
+    public static final IntegerConfiguration DEFAULT_HEARTS = new IntegerConfiguration(id(), "default_hearts", ConfigEnv.SERVER, 1, 100, 10);
+    public static final IntegerConfiguration REVIVE_HEARTS = new IntegerConfiguration(id(), "revive_hearts", ConfigEnv.SERVER, 1, 100, 3);
     public static final IntegerConfiguration REVIVE_DAYS = new IntegerConfiguration(id(), "revive_days", ConfigEnv.SERVER, -1, 100, 30);
     public static final BooleanConfiguration DISABLE_ELYTRA = new BooleanConfiguration(id(), "disable_elytra", ConfigEnv.SERVER, false);
     public static final BooleanConfiguration DISABLE_TOTEMS = new BooleanConfiguration(id(), "disable_totems", ConfigEnv.SERVER, false);
