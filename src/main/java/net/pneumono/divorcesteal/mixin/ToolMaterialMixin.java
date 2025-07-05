f28b7e9e91485acd348e1b2ce60b28d0bfb257ac
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-05 14:58:27 +0100

    Change netherite tool stats to be identical to diamond
    Ideally would be a config/gamerule but oh well.
    This doesn't work properly for netherite hoes, but it makes them deal 1 less damage than diamond ones which is funny as fuck, so it's staying in

---
+++ src/main/java/net/pneumono/divorcesteal/mixin/ToolMaterialMixin.java
@@ -0,0 +1,18 @@
+package net.pneumono.divorcesteal.mixin;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ToolMaterial.class)
+public abstract class ToolMaterialMixin {
+    @SuppressWarnings("unused")
+    @Mutable
+    @Final
+    @Shadow
+    public static ToolMaterial NETHERITE = new ToolMaterial(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, 1561, 8.0F, 3.0F, 10, ItemTags.NETHERITE_TOOL_MATERIALS);
+}
