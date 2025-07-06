f08d3f0b1bb83aa9c105b0907e27bb4dd5d3693d
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-06 11:34:26 +0100

    Add additional data saving to player heads when placed down

---
+++ src/main/java/net/pneumono/divorcesteal/mixin/SkullBlockEntityMixin.java
@@ -0,0 +1,89 @@
+package net.pneumono.divorcesteal.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.SkullBlockEntity;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.ComponentsAccess;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.LoreComponent;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.storage.ReadView;
+import net.minecraft.storage.WriteView;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextCodecs;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SkullBlockEntity.class)
+public class SkullBlockEntityMixin extends BlockEntity {
+    public SkullBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Unique
+    @Nullable
+    private LoreComponent lore;
+    @Unique
+    @Nullable
+    private Text itemName; // Not to be confused with custom_name, they're different I swear!
+    @Unique
+    @Nullable
+    private NbtComponent customData;
+
+    @Inject(
+            method = "writeData",
+            at = @At("RETURN")
+    )
+    private void writeExtraData(WriteView view, CallbackInfo ci) {
+        view.putNullable("lore", LoreComponent.CODEC, this.lore);
+        view.putNullable("item_name", TextCodecs.CODEC, this.itemName);
+        view.putNullable("custom_data", NbtComponent.CODEC, this.customData);
+    }
+
+    @Inject(
+            method = "readData",
+            at = @At("RETURN")
+    )
+    private void readExtraData(ReadView view, CallbackInfo ci) {
+        this.lore = view.read("lore", LoreComponent.CODEC).orElse(null);
+        this.itemName = tryParseCustomName(view, "item_name");
+        this.customData = view.read("custom_data", NbtComponent.CODEC).orElse(null);
+    }
+
+    @Inject(
+            method = "readComponents",
+            at = @At("RETURN")
+    )
+    private void addExtraComponents(ComponentsAccess components, CallbackInfo ci) {
+        this.lore = components.get(DataComponentTypes.LORE);
+        this.itemName = components.get(DataComponentTypes.ITEM_NAME);
+        this.customData = components.get(DataComponentTypes.CUSTOM_DATA);
+    }
+
+    @Inject(
+            method = "addComponents",
+            at = @At("RETURN")
+    )
+    private void addExtraComponents(ComponentMap.Builder builder, CallbackInfo ci) {
+        builder.add(DataComponentTypes.LORE, this.lore);
+        builder.add(DataComponentTypes.ITEM_NAME, this.itemName);
+        builder.add(DataComponentTypes.CUSTOM_DATA, this.customData);
+    }
+
+    @Inject(
+            method = "removeFromCopiedStackData",
+            at = @At("RETURN")
+    )
+    private void removeExtraComponents(WriteView view, CallbackInfo ci) {
+        view.remove("lore");
+        view.remove("item_name");
+        view.remove("custom_data");
+    }
+}
