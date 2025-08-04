46630d6c8801dc6a862f1ee6cca7be9f9654fb8a
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-08-04 03:38:47 +0100

    Fix configs not being registered correctly

--- src/main/java/net/pneumono/divorcesteal/DivorcestealConfig.java
+++ src/main/java/net/pneumono/divorcesteal/DivorcestealConfig.java
@@ -6,19 +6,19 @@ import net.pneumono.pneumonocore.config_api.enums.LoadType;
 
 public class DivorcestealConfig {
     public static final BoundedIntegerConfiguration MAX_HEARTS = register("max_hearts", new BoundedIntegerConfiguration(
-            1, 100, 20, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
+            20, 1, 100, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
     ));
     public static final BoundedIntegerConfiguration DEFAULT_HEARTS = register("default_hearts", new BoundedIntegerConfiguration(
-            1, 100, 10, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
+            10, 1, 100, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
     ));
     public static final BoundedIntegerConfiguration REVIVE_HEARTS = register("revive_hearts", new BoundedIntegerConfiguration(
-            1, 100, 3, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
+            3, 1, 100, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
     ));
     public static final BoundedIntegerConfiguration REVIVE_DAYS = register("revive_days", new BoundedIntegerConfiguration(
-            -1, 100, 30, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
+            30, -1, 100, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
     ));
     public static final BoundedIntegerConfiguration CRAFTED_HEART_LIMIT = register("crafted_heart_limit", new BoundedIntegerConfiguration(
-            1, 100, 7, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
+            7, 1, 100, new ConfigSettings().category("hearts").loadType(LoadType.INSTANT)
     ));
     public static final BooleanConfiguration DISABLE_ELYTRA = register("disable_elytra", new BooleanConfiguration(
             false, new ConfigSettings().category("rebalances").loadType(LoadType.INSTANT)
