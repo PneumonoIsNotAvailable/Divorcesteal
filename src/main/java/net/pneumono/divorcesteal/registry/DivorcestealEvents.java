95ef3e034a091583c2c4c659f42840a09b94dca5
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-10-16 06:43:58 +0100

    Fix death event not properly running for participants

--- src/main/java/net/pneumono/divorcesteal/registry/DivorcestealEvents.java
+++ src/main/java/net/pneumono/divorcesteal/registry/DivorcestealEvents.java
@@ -42,7 +42,7 @@ public class DivorcestealEvents {
     }
 
     private static void afterDeath(Entity entity, DamageSource damageSource) {
-        if (!(entity instanceof ServerPlayerEntity player) || Hearts.isParticipant(player)) return;
+        if (!(entity instanceof ServerPlayerEntity player) || !Hearts.isParticipant(player)) return;
 
         Hearts.addHeartsValidated(player, -1, true);
 
@@ -51,7 +51,6 @@ public class DivorcestealEvents {
                 Hearts.isParticipant(attacker) &&
                 !attacker.getUuid().equals(entity.getUuid())
         ) {
-
             ItemStack headStack = new ItemStack(Items.PLAYER_HEAD);
 
             headStack.set(DataComponentTypes.PROFILE, createProfileComponent(player));
